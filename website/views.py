878837369e0ac04f5da5d2a1a926486ba25980be
CaleaDoraAna <dora.calea99@e-uvt.ro>
2022-01-03 16:34:02 +0200

    Added message flashing.

--- website/views.py
+++ website/views.py
@@ -1,12 +1,23 @@
 '''
 Application blueprint, it stores all the paths to the pages the user will be able to navigate to.
 '''
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/',methods=['GET','POST'])
 def home():
+    if request.method == "POST":
+        data = request.form.get('user-query')
+
+    #check if user-query (ticker) is in database/exists for scraping, if not tell it so
+    # 1) if in database -> display
+    # 2) else if in scraper -> add scraped data to database
+    # 3) else -> not found
+
+    # Error handling - flask has MESSAGE FLASHING, usage flash('sth here', category = 'error/success/etc')
+    #data = request.form
+    #flash(data, category='success')
     return render_template("home.html")
 
 @views.route('/about')
